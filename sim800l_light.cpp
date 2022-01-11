528b375745e4399f093fa9538195a251962121ae
Massimo Frassetto <frassetto.massimoo@gmail.com>
2022-01-11 23:01:02 +0100

    Sessione di stesura

--- sim800l_light.cpp
+++ sim800l_light.cpp
@@ -4,11 +4,21 @@
   comandi AT via seriale.
 */
 
+#include "Arduino.h"
+#include <SoftwareSerial.h>
 #include "sim800l_light.h"  // dichiarazione della classe
 
+SoftwareSerial SIM(RX_PIN,TX_PIN);
+
 /* funzione di inizializzazione */
-void sim800l_light::begin(void)
-{
-  Serial.println("Soccia che figata questa libreria!");
+void sim800l_light::begin(){
+	SIM.begin(9600);
+	#if (LED) 
+		pinMode(OUTPUT,LED_PIN);
+	#endif
+	_buffer.reserve(255); //reserve memory to prevent intern fragmention
 }
 
+// void sim800l_light::ddd(){
+	
+// }
--- sim800l_light.h
+++ sim800l_light.h
@@ -3,14 +3,22 @@
 
 #include "Arduino.h"
 
+#define RX_PIN 10
+#define TX_PIN 11	
+#define RESET_PIN 2   // pin to the reset pin sim800l
+
+#define LED true // used for indicator led, in case that you don want set to false . 
+#define LED_PIN 13 //pin to indicate states. 
+
 class sim800l_light
 {
    private:
-      // qui ci va tutto quello che è private e quindi non visibile da fuori la libreria
+		// qui ci va tutto quello che è private e quindi non visibile da fuori la libreria
+		String _buffer;
 
-   public:
-      // qui ci va tutto quello che deve essere VISIBILE DA FUORI, compreso eventuali variabili di ritorno o tipi di ritorno
-      void begin (void);
+	public:
+		// qui ci va tutto quello che deve essere VISIBILE DA FUORI, compreso eventuali variabili di ritorno o tipi di ritorno
+		void begin (void);
 };
 
 #endif
