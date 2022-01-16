6a9bb3427b6a057f770a8d1066670973e4cd1d5f
Paride <balepari@users.noreply.github.com>
2022-01-16 16:19:13 +0100

    [2BT] 220116-Written some function
    Written some function and docs blocks

--- sim800l_light.cpp
+++ sim800l_light.cpp
@@ -1,24 +1,58 @@
-/*
-  sim800l-light.cpp - Libreria per gestire
-  il modulo SIMCom SIM800L tramite
-  comandi AT via seriale.
-*/
+/**
+ * @file sim800l_light.cpp
+ * @author MakerStation (info@makerstation.it)
+ * @brief A light version of SIM800L communication library using AT commands through serial
+ * @version 0.1
+ * @date 2022-01-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #include "Arduino.h"
 #include <SoftwareSerial.h>
-#include "sim800l_light.h"  // dichiarazione della classe
+#include "sim800l_light.h" // dichiarazione della classe
 
-SoftwareSerial SIM(RX_PIN,TX_PIN);
+SoftwareSerial SIM(RX_PIN, TX_PIN);
 
-/* funzione di inizializzazione */
-void sim800l_light::begin(){
-	SIM.begin(9600);
-	#if (LED) 
-		pinMode(OUTPUT,LED_PIN);
-	#endif
-	_buffer.reserve(255); //reserve memory to prevent intern fragmention
+/**
+ * @brief Set if display or not the result code of any command
+ *
+ */
+void sim800l_light::_setResultCode()
+{
+	SIM.println("ATQ1");
 }
 
-// void sim800l_light::ddd(){
+/**
+ * @brief Set retourned message format
+ *
+ * @param format 0 = PDU format (hex) / 1 = clear text format
+ *
+ */
+void sim800l_light::_setMessageFormat(int format)
+{
+	SIM.println("AT+CMGF=" + format);
+}
+
+/**
+ * @brief Initialization of SIM800 Serial comm and first mandatory setup
+ * 
+ * @param speed Speed of serial (max 19200 to avoid packet loss)
+ */
+void sim800l_light::begin(int speed)
+{
+	if (speed > 19200)
+	{
+		speed = 19200;
+	}
 	
+	SIM.begin(speed);
+#if (LED)
+	pinMode(OUTPUT, LED_PIN);
+#endif
+	_buffer.reserve(255); // reserve memory to prevent intern fragmention
+}
+
+
 // }
--- sim800l_light.h
+++ sim800l_light.h
@@ -10,15 +10,21 @@
 #define LED true // used for indicator led, in case that you don want set to false . 
 #define LED_PIN 13 //pin to indicate states. 
 
+const int iMsg_PDU_Hex_Format = 0;
+const int iMsg_Text_Format = 1;
+
 class sim800l_light
 {
    private:
 		// qui ci va tutto quello che è private e quindi non visibile da fuori la libreria
 		String _buffer;
+		void _setResultCode (void);
+		void _setMessageFormat (int format);
+
 
 	public:
 		// qui ci va tutto quello che deve essere VISIBILE DA FUORI, compreso eventuali variabili di ritorno o tipi di ritorno
-		void begin (void);
+		void begin (long speed);
 };
 
 #endif
