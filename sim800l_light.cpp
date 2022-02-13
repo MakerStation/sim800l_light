072421ecc2c679111d1e917ec73afaeeb825c296
Paride <balepari@users.noreply.github.com>
2022-02-13 22:12:38 +0100

    2021.02.13 - Funzioni e sistemazioni
    * Sistemato codici funzioni
    * modificato in modo da togliere la definizione della softwareserial dentro la libreria e demandata al maincode.
    * Implementato funzione di controllo per begin()
    * aggiunto prototipi funzioni da scrivere
    * aggiunto/modificato variabili
    * aggiunto descrizioni funzioni in Doxygen style

--- sim800l_light.cpp
+++ sim800l_light.cpp
@@ -10,50 +10,183 @@
  */
 
 #include "Arduino.h"
-#include <SoftwareSerial.h>
-#include "sim800l_light.h" // dichiarazione della classe
+// #include <SoftwareSerial.h>	// passing the serial object to function, the serial definition must be done in main code, so #include is not needed
+#include "sim800l_light.h"
+#include <avr/sleep.h>
 
-SoftwareSerial SIM(RX_PIN, TX_PIN);
+// SoftwareSerial SIM(RX_PIN, TX_PIN);	// passing the serial object to function, the serial definition must be done in main code
 
 /**
- * @brief Set if display or not the result code of any command
+ * @brief Enable the showing of result code of any command
  *
+ * @param serobj The serial object to use to communicate with SIM800L
+ * @return true The command completed successfully
+ * @return false Something goes wrong with the command
  */
-void sim800l_light::_enableResultCode()
+bool sim800l_light::_enableResultCode(SoftwareSerial &serobj)
 {
-	SIM.println("ATQ0");
-	// qui ci va il controllo che sulla seriale ritorni "OK"
+	serobj.println(F("ATQ0"));
+	if (serobj.available())
+	{
+		if (serobj.readString().indexOf("OK") != -1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
 
 /**
- * @brief Set retourned message format
+ * @brief Set returned message format
  *
- * @param format 0 = PDU format (hex) / 1 = clear text format
- *
+ * @param format 0/1 - 0 = PDU format (hex) / 1 = Clear Text
+ * @param serobj The serial object to use to communicate with SIM800L
+ * @return true
+ * @return false
  */
-void sim800l_light::_setMessageFormat(int format)
+bool sim800l_light::_setMessageFormat(int format, SoftwareSerial serobj)
 {
-	SIM.println("AT+CMGF=" + format);
+	serobj.println(F("AT+CMGF=") + format);
+	if (serobj.available())
+	{
+		if (serobj.readString().indexOf("OK") != -1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+/**
+ * @brief Try to connect to SIM800L module
+ *
+ * @param serobj The serial object to use to communicate with SIM800L
+ * @return true Connect ok
+ * @return false Connect not ok
+ */
+bool sim800l_light::_tryToConnect(SoftwareSerial &serobj)
+{
+
+	for (_iConnectTest = 0; _iConnectTest < 3; _iConnectTest++)
+	{
+		delay(10000);
+		serobj.println(F("AT"));
+		if (serobj.available())
+		{
+			if (serobj.readString().indexOf("OK") != -1)
+			{
+				_bInitDone = true;
+			}
+			else
+			{
+				_bInitDone = false;
+			}
+		}
+
+		switch (_bInitDone)
+		{
+		case true:
+			// doing all SIM800L init procedure needed to work correctly
+			if (_enableResultCode(serobj))
+			{
+				if (_setMessageFormat(1, serobj))
+				{
+					return true;
+				}
+			}
+			else
+			{
+				return false;
+			}
+			break;
+		case false:
+			// do SIM800L reset through the dedicated pin
+			pinMode(RESET_PIN, OUTPUT);
+			digitalWrite(RESET_PIN, LOW);
+			delay(500);
+			digitalWrite(RESET_PIN, HIGH);
+		default:
+			break;
+		}
+	}
+
+	/* the following code will "stop" the execution of the code, preventing function to return the false state */
+	if (_iConnectTest > 2)
+	{
+		Serial.println("   ****** W A R N I N G ******");
+		Serial.println("There was a problem during SIM800L Module init procedure.");
+		Serial.println("Module didn't respond after 10 sec and it has been already resetted 2 times...");
+		Serial.println("Check SIM800L Module, power and connection.");
+		Serial.println("");
+		Serial.println("=== PROGRAM HALTED ===");
+		// as a very HALT state doesn't exist in arduino (you can only power off it), we use deep sleep so to reduce drastically power consumption
+		sleep_enable();						 // enabling sleep function
+		set_sleep_mode(SLEEP_MODE_PWR_DOWN); // setting sleep mode
+		delay(1000);						 // time wait 1 sec - could be removed or reduced
+		power_all_disable();				 // disabling all (btw it will disable also interrupts!)
+		sleep_mode();						 // going to sleep
+	}
 }
 
 /**
  * @brief Initialization of SIM800 Serial comm and first mandatory setup
- * 
+ *
  * @param speed Speed of serial (max 19200 to avoid packet loss)
  */
-void sim800l_light::begin(long speed)
+void sim800l_light::begin(long speed, SoftwareSerial &serobj)
 {
-	if (speed > 19200)
+	switch (speed)
 	{
+	case 300:
+		speed = 300;
+		break;
+	case 600:
+		speed = 600;
+		break;
+	case 1200:
+		speed = 1200;
+		break;
+	case 2400:
+		speed = 2400;
+		break;
+	case 4800:
+		speed = 4800;
+		break;
+	case 9600:
+		speed = 9600;
+		break;
+	case 19200:
 		speed = 19200;
+		break;
+	default:
+		speed = 19200;
+		bPrintSerialWarning = true;
+		break;
 	}
-	
-	SIM.begin(speed);
+
+	serobj.begin(speed);
+	while (!serobj)
+	{
+		;
+	}
+	// qui ci andrebbe funzione di controllo se entro 10 sec il modulo gsm ha risposto oppure no.
+	// si potrebbe usare la lib elapsedMillis https://github.com/pfeerick/elapsedMillis
+	// perchè se dopo 10 sec il moulo gsm non risponde occorre fargli un reset hw con il suo pin
+	// oppure fargli un power cycle.
+	// però mettere un delay() fa schifo perchè il programma si blocca per il tempo della delay()
+	// e non è bellissimo....
+	if (_tryToConnect(serobj))
+	{
+		_buffer.reserve(255); // reserve memory to prevent intern fragmention
+	}
+¯
 #if (LED)
 	pinMode(OUTPUT, LED_PIN);
 #endif
-	_buffer.reserve(255); // reserve memory to prevent intern fragmention
 }
-
-
-// }
--- sim800l_light.h
+++ sim800l_light.h
@@ -10,6 +10,31 @@
 #define LED true // used for indicator led, in case that you don want set to false . 
 #define LED_PIN 13 //pin to indicate states. 
 
+enum messageStatus
+{
+	"REC UNREAD",
+	"REC READ",
+	"ALL"
+};
+
+enum deleteType
+{
+	"DEL READ",		// delete all messages that have been READ
+	"DEL UNREAD",	// delete all messages that are still TO BE READ
+	"DEL SENT",		// delete all messages that have been sent
+	"DEL UNSENT",	// delete all messages that are still TO BE SEND (draft)
+	"DEL INBOX",	// delete all received messages
+	"DEL ALL"		// delete ALL messages from any storage
+};
+
+enum cmdType
+{
+	"L0GIN",
+	"Reset",
+	"Dati",
+	"Orario"
+};
+
 const int iMsg_PDU_Hex_Format = 0;
 const int iMsg_Text_Format = 1;
 
@@ -18,13 +43,24 @@ class sim800l_light
    private:
 		// qui ci va tutto quello che è private e quindi non visibile da fuori la libreria
 		String _buffer;
-		void _enableResultCode();
-		void _setMessageFormat(int format);
+		bool _bPrintSerialWarning;
+		bool _bInitDone;
+		int _iConnectionTest;
+		
+		bool _enableResultCode(SoftwareSerial &serobj);
+		bool _setMessageFormat(int format, SoftwareSerial &serobj);
+		bool _tryToConnect(SoftwareSerial &serobj);
+		string _readSMS(cmdType commandToSearch, SoftwareSerial &serobj, bool changeMe);	//TODO richiamata da checkMessageForCommand
 
 
 	public:
 		// qui ci va tutto quello che deve essere VISIBILE DA FUORI, compreso eventuali variabili di ritorno o tipi di ritorno
-		void begin(long speed);
+		void begin(long speed, SoftwareSerial &serobj);
+		int getMessageCountByType(messageStatus stat, bool changeMe, SoftwareSerial &serobj);	// TODO
+		bool deleteMessage(deleteType delType, SoftwareSerial &serobj);		// TODO
+		void checkMessageForCommand(cmdType command, SoftwareSerial & serobj);	// TODO
+		bool sendSMS(string dest, string text, SoftwareSerial &Serobj);		//TODO - Ricordarsi il CTRL+Z alla fine (ASCII 26) altrimenti il messaggio non va via
+		int checkForMessage(SoftwareSerial &serobj);	// TODO - controlla il +CMTI sulla seriale
 };
 
 #endif
